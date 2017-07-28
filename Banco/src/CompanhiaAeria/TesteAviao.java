9615d15802cbd4812154ec96caa783dd0e94e04d
Noturno <Noturno@192.168.7.22>
2017-07-28 20:00:12 -0300

    commit exercicios corrigidos

--- Banco/src/CompanhiaAeria/TesteAviao.java
+++ Banco/src/CompanhiaAeria/TesteAviao.java
@@ -4,7 +4,7 @@ public class TesteAviao {
 
 	public static void main(String[] args) {
 		try {
-			Boing boing = new Boing(0, 500d);
+			Boing boing = new Boing(5, 500d);
 			Boing boing2 = new Boing(30, 1000d);
 		} catch (IllegalArgumentException e) {
 			System.out.println(e.getMessage());
